80f9785f8da95f1741373f5b5f3d90dc08e4bce8
goguy <goguy@naver.com>
2022-08-24 16:54:24 +0900

    environ add

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,4 @@
+import environ
 from .base import *
 
 ALLOWED_HOSTS = ['3.39.163.171']
@@ -8,16 +9,19 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'ls-b9a6b15c7619fb1859ed7abccf36ea2967b9a8ee.c36vvyyhk7pk.ap-northeast-2.rds.amazonaws.com',
-        'NAME':'pybodb',
-        'USER': 'dbmasteruser',
-        'PASSWORD' : 'P9I^=1RTTl^ya]I{*Y[~2u[.tVusgoQe',
+        'HOST': env('DB_HOST'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD' : env('DB_PASSWORD'),
         'PORT' : 5432
     }
 }
